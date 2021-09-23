b834906f65da2108e9539981d9fdf01b83ef0328
samuelhurtado20 <samuelhurtado20@hotmail.com>
2021-09-22 21:03:26 -0500

    first approximation

---
+++ FluentValidation_Net_5/PersonValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentValidation_Net_5
+{
+	class PersonValidator : AbstractValidator<Person>
+	{
+		public PersonValidator()
+		{
+			RuleFor(x => x.Id).NotNull();
+			RuleFor(x => x.Name).NotEmpty().MinimumLength(3).MaximumLength(15).WithMessage(@"Invalid Name");
+			RuleFor(x => x.Email).EmailAddress().NotEmpty();
+			RuleFor(x => x.Age).InclusiveBetween(18, 65);
+		}
+	}
+}
---
+++ FluentValidation_Net_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FluentValidation_Net_5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person = new();
+			var validator = new PersonValidator();
+			var results = validator.Validate(person);
+
+			if (!results.IsValid)
+			{
+				foreach (var failure in results.Errors)
+				{
+					Console.WriteLine("Property " + failure.PropertyName + " failed validation. Error was: " + failure.ErrorMessage);
+				}
+			}
+
+			string allMessages = results.ToString('~'.ToString());
+			Console.WriteLine(allMessages);
+
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+		}
+	}
+}
